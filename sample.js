586d47d7e468789bc12ca3e8a5372e4a9a3f09a5
saltground <saltground@gmail.com>
2016-11-10 20:19:42 +0900

    update by saltground

--- sample.js
+++ sample.js
@@ -1,3 +1,4 @@
 const message = 'hello world from nishitani';
 const message2 = 'hello world from nishitani, again';
 const message2 = 'good night world from giovanni, again';
+const message3 = 'hello world from shiocchi';
